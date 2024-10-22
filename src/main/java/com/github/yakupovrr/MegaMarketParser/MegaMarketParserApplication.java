f60cbd10b36bf3148609770c37976e9ea8477ef6
YakupovRR <specter63@gmail.com>
2024-10-22 15:04:42 +0400

    Initial commit

---
+++ src/main/java/com/github/yakupovrr/MegaMarketParser/MegaMarketParserApplication.java
@@ -0,0 +1,13 @@
+package com.github.yakupovrr.MegaMarketParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaMarketParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaMarketParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yakupovrr/MegaMarketParser/MegaMarketParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yakupovrr.MegaMarketParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaMarketParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
